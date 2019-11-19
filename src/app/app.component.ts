bdbeb9e988e1cfb74972193b5e6ca8a41b83eb9c
Andres Weber <andresmweber@gmail.com>
2019-11-18 18:47:17 -0700

    Finished through tutorial 5.  He messed up on class decorators, need to follow up myself.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,34 @@
 import { Component } from '@angular/core';
 
+function logClass(decoratedClass){
+  console.log(decoratedClass)
+  return (...args) => {
+    console.log("logClass Args ", args)
+    return new decoratedClass(...args)
+  }
+}
+
+function log(target, name, descriptor){
+  console.log(target, name, descriptor)
+  const original = descriptor.value
+  descriptor.value = function(...args){
+    console.log('Args ', args)
+    const result = original.apply(this, args)
+    console.log('Result ', result)
+    return result
+  }
+  return descriptor
+}
+
+// @logClass - He messed this up, will need to figure out class decorators on my own.
+class ExampleClass {
+  constructor(arg1, arg2){
+    console.log("Constructed with ", arg1, arg2)
+  }
+}
+
+const myClass = new ExampleClass(5,10)
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +36,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-freecodecamp';
+
+  constructor(){ 
+    this.aSimpleMethod(5, 2)
+  }
+
+  @log
+  aSimpleMethod(a, b){
+    return a*b
+  }
 }
